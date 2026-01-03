34d3772eb0fd830216d2bdc52aacd6cf5ee7eb18
thilakkathir67 <thilakkathir67@gmail.com>
2026-01-03 08:37:41 +0530

    calculator

---
+++ python/calculator.py
@@ -0,0 +1,16 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=input("Enter operator:")
+if(c=='+'):
+    print(a+b)
+elif(c=='-'):
+    print(a-b)
+elif(c=='*'):
+    print(a*b)
+elif(c=='/'):
+    if(b!=0):
+        print(a/b)
+    else:
+        print("division not possible") #if you use a//b no decimal will come in value
+else:
+    print("invalid operator");
